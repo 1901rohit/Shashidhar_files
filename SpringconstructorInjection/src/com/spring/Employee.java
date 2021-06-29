117d679e2db13a4ef62ab7f846a16bd532eca3a8
1901rohit <lokerevushasi@gmail.com>
2021-06-29 22:57:14 +0530

    SpringConstructorInjection

---
+++ SpringconstructorInjection/src/com/spring/Employee.java
@@ -0,0 +1,32 @@
+package com.spring;
+
+public class Employee {
+	private int id;
+	private String name;
+	
+	
+	
+	public Employee(int id, String name) {
+		
+		this.id = id;
+		this.name = name;
+	}
+
+
+
+
+	public void show()
+	{
+		System.out.println("Employee ID: "+id);
+		System.out.println("Employee Name: "+name);
+	}
+		
+
+		
+		
+		
+		
+
+	
+
+}
